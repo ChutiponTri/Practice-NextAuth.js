6f354bc0cb386f2287ed6a43d60150a7f0da53e8
ChutiponTri <chutipon.trir@gmail.com>
2025-05-22 16:55:37 +0700

    Relocate AuthProviders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { AuthProvider } from "./Providers";
+import { AuthProvider } from "@/components/AuthProviders";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
